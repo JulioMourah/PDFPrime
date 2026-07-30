ebf1939c115f934edf1b8540a894655a773b17d1
Julio Mourah <contato.arq@juliomourah.com>
2026-07-30 13:45:24 -0300

    Preparando backend para deploy no Render

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
     console.log(`🚀 Backend rodando na porta ${PORT}`);
 
