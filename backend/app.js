2b94331bd7662a71cfa38d2a5ff185f172dfe542
Julio Mourah <contato.arq@juliomourah.com>
2026-08-03 15:26:04 -0300

    Teste grupo 2

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,12 @@ const pdfToPngRoutes = require("./routes/pdfToPngRoutes");
 const mergePdfRoutes = require("./routes/mergePdfRoutes");
 const splitPdfRoutes = require("./routes/splitPdfRoutes");
 
+const compressPdfRoutes = require("./routes/compressPdfRoutes");
+const rotatePdfRoutes = require("./routes/rotatePdfRoutes");
+const reorderPdfRoutes = require("./routes/reorderPdfRoutes");
+const protectPdfRoutes = require("./routes/protectPdfRoutes");
+const unlockPdfRoutes = require("./routes/unlockPdfRoutes");
+
 const app = express();
 
 app.use(cors());
@@ -17,6 +23,12 @@ app.use("/api", pdfToJpgRoutes);
 app.use("/api", pdfToPngRoutes);
 app.use("/api", mergePdfRoutes);
 app.use("/api", splitPdfRoutes);
+
+app.use("/api", compressPdfRoutes);
+app.use("/api", rotatePdfRoutes);
+app.use("/api", reorderPdfRoutes);
+app.use("/api", protectPdfRoutes);
+app.use("/api", unlockPdfRoutes);
 
 app.get("/", (req, res) => {
     res.json({
