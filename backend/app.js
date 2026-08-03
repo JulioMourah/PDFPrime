7c0206b75ba929ff94042c2f499aa69a28017a38
Julio Mourah <contato.arq@juliomourah.com>
2026-08-03 15:17:39 -0300

    Teste primeira rota

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
+const imageToPdfRoutes = require("./routes/imageToPdfRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", imageToPdfRoutes);
+
 app.get("/", (req, res) => {
-
     res.json({
-
         status: "API funcionando"
-
     });
-
 });
 
 module.exports = app;
