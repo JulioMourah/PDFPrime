57ddbdb5e10a88bce8ae2071d1adf11737269b42
Julio Mourah <contato.arq@juliomourah.com>
2026-08-06 09:54:00 -0300

    Corrige QPDF para Linux

--- backend/services/protectPdfService.js
+++ backend/services/protectPdfService.js
@@ -1,6 +1,9 @@
 const { execFile } = require("child_process");
 
-const QPDF = "C:\\Program Files\\qpdf 12.3.2\\bin\\qpdf.exe";
+const QPDF =
+    process.platform === "win32"
+        ? "C:\\Program Files\\qpdf 12.3.2\\bin\\qpdf.exe"
+        : "qpdf";
 
 function protectPdf(inputPath, outputPath, password) {
 
